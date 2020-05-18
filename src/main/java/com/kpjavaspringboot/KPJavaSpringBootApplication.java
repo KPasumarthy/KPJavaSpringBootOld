ef6d3d0fa568d7854a301fa407d94ad215316aa0
Kailash Pasumarthy <KPasumarthy@gmail.com>
2020-05-18 17:31:27 -0400

    KP : KPJavaSpringBoot App & REST APIs - Initial Commit!

---
+++ src/main/java/com/kpjavaspringboot/KPJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kpjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KPJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KPJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpjavaspringboot/KpJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
