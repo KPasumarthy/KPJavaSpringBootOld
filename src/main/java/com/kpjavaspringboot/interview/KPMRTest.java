87958339878623a4c93018741e1eef8a26726065
Kailash Pasumarthy <KPasumarthy@gmail.com>
2020-11-25 19:17:11 -0500

    KP : Return Non-Repetitive Elements in Array

--- src/main/java/com/kpjavaspringboot/interview/KPMRTest.java
+++ src/main/java/com/kpjavaspringboot/interview/KPMRTest.java
@@ -33,14 +33,12 @@ public class KPMRTest {
 			if (hm.get(a) == null) {
 				i = 0;
 				hm.put(a, ++i);
-				System.out.printf("HM : a=%d, i=%d \n", a, i);
+				//System.out.printf("HM : a=%d, i=%d \n", a, i);
 			} else {
 				i = (int) hm.get(a);
 				hm.put(a, ++i);
-				System.out.printf("HM : a=%d, i=%d \n", a, i);
+				//System.out.printf("HM : a=%d, i=%d \n", a, i);
 			}
-
-			// i = 0;
 
 			if (!hs.contains(a)) {
 				hs.add(a);
@@ -61,8 +59,7 @@ public class KPMRTest {
 		while (hmIterator.hasNext()) {
 			Map.Entry mapElement = (Map.Entry) hmIterator.next();
 			int value = ((int) mapElement.getValue());
-			System.out.println(
-					"The HashMapElement : Key =  " + mapElement.getKey() + " Value = " + mapElement.getValue());
+			//System.out.println( "The HashMapElement : Key =  " + mapElement.getKey() + " Value = " + mapElement.getValue());
 
 			if (value == 1) {
 				hsUnq.add(mapElement.getKey());
@@ -72,27 +69,10 @@ public class KPMRTest {
 		}
 
 		System.out.println("The Unique HashMap : " + hm);
+		System.out.println("The HashSet: " + hs);
 		System.out.println("The Non-Duplicate Unique HashSet : " + hsUnq);
 		System.out.println("The Duplicate & Repetitive HashSet : " + hsRep);
-
-		System.out.println("The HashSet: " + hs);
-		//System.out.println(hs.toArray().toString());
-		// Trial : 1 - Ramani.Mukesh@gmail.com
-
-		/*
-		 * ///Hash Sets of Duplicates and Non-Duplicate HashSet set = new HashSet();
-		 * Set<Integer> dup = new HashSet<Integer>(); Set<Integer> noDup = new
-		 * HashSet<Integer>(); for (int a : A) { if (set.add(a) == false) { // // your
-		 * duplicate element System.out.printf("\t Duplicate Value of 'a' : %d \n", a);
-		 * dup.add(a); } else { System.out.printf("\t Unique : Value of 'a' : %d \n",
-		 * a); noDup.add(a); } }
-		 * 
-		 * System.out.println("The Duplicate HashSet: " + dup);
-		 * System.out.println("The Non-Duplicate HashSet: " + noDup);
-		 */
-		
-		// Creating the array and using toArray() 
-        //Integer[] arr = (Integer[]) hsUnq.toArray();
+	
 		
 	}
 
